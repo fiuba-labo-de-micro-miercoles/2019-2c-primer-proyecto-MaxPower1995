0e9e164487462923c1cf6aafaefaeea48422be7a
MaxPower1995 <65687057+MaxPower1995@users.noreply.github.com>
2020-05-29 19:37:11 -0300

    columnados

--- Trabajos_Practicos/Archivos ASM/TP1_usando_todo_el_puerto.asm
+++ Trabajos_Practicos/Archivos ASM/TP1_usando_todo_el_puerto.asm
@@ -1,4 +1,3 @@
-;
 ; Trabajo_Practico_1.asm
 ; Encendido de un LED
 ; Created: 16/5/2020 10:24:57
@@ -15,44 +14,44 @@ para que el led encienda.
 
 .include "m328pdef.inc" 
 
-.cseg					;Escribo el codigo en la Memoria FLASH
+.cseg						;Escribo el codigo en la Memoria FLASH
 .org 0x0000
 
 jmp main
 
-.org INT_VECTORS_SIZE   ;Calcula cuanta memoria deja para los perifericos segun la placa
-						;Esa cte esta difinida en m328pdef.inc
+.org INT_VECTORS_SIZE		;Calcula cuanta memoria deja para los perifericos segun la placa
+							;Esa cte esta difinida en m328pdef.inc
 
 main:
 
 ;Configuro el Puerto B del Micro
 
-ldi R16,0xFF		    ;Carga en el registro 16 todos 1 -> 0b11111111
-ldi R17,0x00
-out	DDRB,R16			;Configuro todo el puerto B como salida
+	ldi R16,0xFF		    ;Carga en el registro 16 todos 1 -> 0b11111111
+	ldi R17,0x00
+	out	DDRB,R16			;Configuro todo el puerto B como salida
 
 ciclo_infinito:
 
-out PORTB,R16
-rcall tiempo_espera     ;Espera el tiempo configurado en esa Bandera
-out PORTB,R17
-rcall tiempo_espera
-rjmp ciclo_infinito     ; Vuelve a iniciar este loop infinito
+	out PORTB,R16
+	rcall tiempo_espera     ;Espera el tiempo configurado en esa Bandera
+	out PORTB,R17
+	rcall tiempo_espera
+	rjmp ciclo_infinito		; Vuelve a iniciar este loop infinito
 
-tiempo_espera:			; confiro el tiempo de espera
+tiempo_espera:				; confiro el tiempo de espera
 
 	ldi R20, 32
 t3: ldi R19, 250
 t2: ldi R18, 250
-t1: nop					; hasta aca son 4 ciclos de maquina por iteracion -> 250ns
+t1: nop						; hasta aca son 4 ciclos de maquina por iteracion -> 250ns
 
-dec R18					; 250 x 250ns = 62.5us
-brne t1
+	dec R18					; 250 x 250ns = 62.5us
+	brne t1
 
-dec R19
-brne t2					; 250 x 50us = 15.625ms
+	dec R19
+	brne t2					; 250 x 50us = 15.625ms
 
-dec R20
-brne t3					; 32 x 15.625ms = 500ms 
-						; Con esto logre un retardo de Medio Segundo
-ret						; Vuelve a la linea siguiente de la que fue llamado
+	dec R20
+	brne t3					; 32 x 15.625ms = 500ms 
+							; Con esto logre un retardo de Medio Segundo
+	ret						; Vuelve a la linea siguiente de la que fue llamado
--- Trabajos_Practicos/Archivos ASM/TP1_usando_un_pin.asm
+++ Trabajos_Practicos/Archivos ASM/TP1_usando_un_pin.asm
@@ -11,43 +11,43 @@ con un 1 logico ese pin para que encienda y con un 0 logico para que apague el l
 
 .include "m328pdef.inc" 
 
-.cseg					    ;Escribo el codigo en la Memoria FLASH
+.cseg							;Escribo el codigo en la Memoria FLASH
 .org 0x0000
 
 jmp main
 
-.org INT_VECTORS_SIZE       ;Calcula cuanta memoria deja para los perifericos segun la placa
-						    ;Esa cte esta difinida en m328pdef.inc
+.org INT_VECTORS_SIZE			;Calcula cuanta memoria deja para los perifericos segun la placa
+								;Esa cte esta difinida en m328pdef.inc
 
 main:
 
 ;Configuro el Puerto B del Micro
 
-ldi R16,0b00000001			;Carga en el registro 16 -> 0b00000001
-out	DDRB,R16			    ;Configuro solo el pin 0 del puerto b como salida
+	ldi R16,0b00000001			;Carga en el registro 16 -> 0b00000001
+	out	DDRB,R16			    ;Configuro solo el pin 0 del puerto b como salida
 
 ciclo_infinito:
 
-sbi PORTB,0					;Configura un valor alto en P0 -> Enciende el LED
-rcall tiempo_espera		    ;Espera el tiempo configurado en esa Bandera
-cbi PORTB,0			        ;Configura un valor bajo en el P0 -> Apaga el LED
-rcall tiempo_espera
-rjmp ciclo_infinito         ;Vuelve a iniciar este loop infinito
+	sbi PORTB,0					;Configura un valor alto en P0 -> Enciende el LED
+	rcall tiempo_espera		    ;Espera el tiempo configurado en esa Bandera
+	cbi PORTB,0			        ;Configura un valor bajo en el P0 -> Apaga el LED
+	rcall tiempo_espera
+	rjmp ciclo_infinito         ;Vuelve a iniciar este loop infinito
 
-tiempo_espera:			    ;configuro el tiempo de espera clock de 16Mhz
+tiempo_espera:					;configuro el tiempo de espera clock de 16Mhz
 
 	ldi R20, 32
 t3: ldi R19, 250
 t2: ldi R18, 250			
-t1: nop					    ; hasta aca son 4 ciclos de maquina por iteracion -> 250ns
+t1: nop							; hasta aca son 4 ciclos de maquina por iteracion -> 250ns
 
-dec R18					    ; 250 x 250ns = 62.5us
-brne t1
+	dec R18					    ; 250 x 250ns = 62.5us
+	brne t1
 
-dec R19
-brne t2					    ; 250 x 62.5us = 15.625ms
+	dec R19
+	brne t2					    ; 250 x 62.5us = 15.625ms
 
-dec R20
-brne t3					    ; 32 x 15.625us = 500ms 
-						    ; Con esto logre un retardo de Medio Segundo
-ret							; Avanza a la linea siguiente de codigo que lo llamo
+	dec R20
+	brne t3					    ; 32 x 15.625us = 500ms 
+								; Con esto logre un retardo de Medio Segundo
+	ret							; Avanza a la linea siguiente de codigo que lo llamo
